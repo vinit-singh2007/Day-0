991b414aa70a94141e28fc802d90ed36c08400b9
vinit-singh2007 <rajputvinitsingh2007@gmail.com>
2026-09-05 14:53:22 +0530

    Backend ready for Render deployment

--- backend/src/config/Mongodb_connect.js
+++ backend/src/config/Mongodb_connect.js
@@ -2,18 +2,28 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const baseURL=process.env.MONGODB_URI
+    const baseURL = process.env.MONGODB_URI;
+
     if (!baseURL) {
-      throw new Error("MONGODB_URI is not defined in environment variables.");
+      throw new Error(
+        "MONGODB_URI is not defined in environment variables."
+      );
     }
-    const conn = await mongoose.connect(baseURL);
+
+    console.log("MongoDB URI found. Connecting...");
+
+    const conn = await mongoose.connect(baseURL, {
+      serverSelectionTimeoutMS: 5000,
+    });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error connecting to MongoDB: ${error.message}`);
-    // Exit process with failure code if DB connection fails
-    process.exit(1);
-  }
+  console.error("MongoDB connection failed");
+  console.error("Name:", error.name);
+  console.error("Message:", error.message);
+  console.error("Reason:", error.reason);
+  process.exit(1);
+}
 };
 
 export default connectDB;
--- backend/src/services/auth.js
+++ backend/src/services/auth.js
@@ -2,23 +2,27 @@ import jwt from "jsonwebtoken";
 
 const setUser = (newUser) => {
   const secretkey = process.env.JWT_SECRET_KEY;
-  // Payload me _id aur id dono bhej rahe hain taaki controller me confusion na ho
+
+  // console.log("JWT_SECRET_KEY exists:", !!secretkey);
+
   return jwt.sign(
-    { 
-      _id: newUser._id, 
-      id: newUser._id, 
-      email: newUser.email 
+    {
+      _id: newUser._id,
+      id: newUser._id,
+      email: newUser.email,
     },
     secretkey,
-    { expiresIn: "3d" }
+    {
+      expiresIn: "3d",
+    }
   );
 };
 
 const getUser = (token) => {
   if (!token) return null;
-  
-  const secretkey = process.env.JWT_SECRET_KEY; // <-- Yahan secretkey add kar di
-  
+
+  const secretkey = process.env.JWT_SECRET_KEY;
+
   try {
     return jwt.verify(token, secretkey);
   } catch (error) {
